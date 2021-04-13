90dce402338255beaf6695517c95faec570b3a02
MurathanGoktas <murathangoktas@gmail.com>
2021-04-13 16:23:47 -0700

    trash debug code deleted

--- MIBFPair/MIBFPairContigs.cpp
+++ MIBFPair/MIBFPairContigs.cpp
@@ -13,17 +13,10 @@ unsigned findContigBS(vector<unsigned> &m_pos, unsigned first, unsigned last, un
 	unsigned middle;
 	if(last >= first)
 	{
-		//std::cout << "first: " << first << std::endl;
-		//std::cout << "last: " << last << std::endl;
 		if(first + 1 == last){
 			return first;
 		}
 		middle = (first + last)/2;
-		//Checking if the element is present at middle loc
-		//if(m_pos[middle] == search_pos){
-		//	return middle+1;
-		//}
-		//Checking if the search element is present in greater half
 		if(search_pos == m_pos[first]){
 			return first;
 		}
@@ -43,8 +36,6 @@ unsigned getEdgeDistances(vector<unsigned> &m_pos, unsigned search_pos, unsigned
 	contig_id = findContigBS(m_pos, 0, m_pos.size() - 1, search_pos);
 	start_dist = search_pos - m_pos[contig_id];
 	end_dist = m_pos[contig_id + 1] - search_pos;
-	//std::cout << "cid\tc_start_pos\tsearch_pos\tstart_dist\tend_dist\n";
-	//std::cout << contig_id << "\t" << m_pos[contig_id] << "\t" << search_pos << "\t" << start_dist << "\t" << end_dist << std::endl;
 	return contig_id;
 }
 
@@ -85,10 +76,9 @@ int main(int argc, char** argv) {
 		m_pos.push_back(std::stoi(str));
 		//std::cout << "m: " << m << std::endl;
 	}
-	contig_count = m_pos.size() - 1;
+	contig_count = m_pos.size() - 1; // last elements is last index of last contig + 1, not starting pos of any contig
 	/// read string pos vector --------
 
-	std::cout << "test 99" << std::endl;
 	/// creating contig length vector --------
 	//unsigned prev_pos = m_pos[0];
 	vector<unsigned> m_length;
@@ -118,26 +108,6 @@ int main(int argc, char** argv) {
 	for(unsigned i = 0; i < hit_map.size(); i++){
 		hit_map[i].resize(contig_count);	
 	}
-	//test
-	//int sum = 0;
-	//for(int m = 0; m < contig_count; m++){
-	//	for(int k = 0; k < contig_count; k++){
-	//		for(int l = 0; l < 8; l++){
-	//			sum += hit_map[m][k][l];
-	//		}
-	//	}
-	//}
-	//if(sum == 0){
-	//	std::cout << "TEST SUCCESSFULL!\n";
-	//}
-	/// init size of vectors --------
-
-	//std::cout << findContigBS(m_pos,0,m_pos.size()-1,5) << std::endl;
-	//std::cout << findContigBS(m_pos,0,m_pos.size()-1,500) << std::endl;
-	//std::cout << findContigBS(m_pos,0,m_pos.size()-1,30000) << std::endl;
-	//std::cout << findContigBS(m_pos,0,m_pos.size()-1,120000) << std::endl;
-
-	//return 0;
 	
 	unsigned kmer_dist = d_arg * 1.2;
 	unsigned expected_total_edge_dist = d_arg * 0.2;
@@ -151,16 +121,12 @@ int main(int argc, char** argv) {
 			if(m_filter.at_rank(*itr1,m_rank_pos_1) && m_filter.at_rank(*itr2,m_rank_pos_2)){
 				m_data_1 = m_filter.get_data(m_rank_pos_1);
 				m_data_2 = m_filter.get_data(m_rank_pos_2);
-				//all_saturated = false;
-				//all_same = true;
 				for(unsigned m = 0; m < m_filter.get_hash_num(); m++){
 					if(m_data_1[m] > m_filter.MASK){
-						//++total_unsaturated_rank;
 						continue;
 					}
 					for(unsigned k = 0; k < m_filter.get_hash_num(); k++){
 						if(m_data_2[k] > m_filter.MASK){
-							//++total_unsaturated_rank;
 							continue;
 						}
 						c_1 = getEdgeDistances(m_pos,m_data_1[m],start_dist_1,end_dist_1);
@@ -168,11 +134,8 @@ int main(int argc, char** argv) {
 						if(c_1 == c_2){
 							continue;
 						}
-		//				std::cout << "c1c2\t" << c_1 << "\t" << c_2 << std::endl;
-		//				std::cout << start_dist_1 << "\t" << end_dist_1 << "\t" << start_dist_2 << "\t" << end_dist_2 << std::endl;
 						if(end_dist_1 + start_dist_2 < expected_total_edge_dist + error_margin && end_dist_1 + start_dist_2 > expected_total_edge_dist - error_margin){
 							c_1 < c_2 ? ++hit_map[c_1][c_2][0] : ++hit_map[c_2][c_1][2];
-							//++hit_map[c_1][c_2][0];
 						}else{
 							c_1 < c_2 ? ++hit_map[c_1][c_2][1] : ++hit_map[c_2][c_1][3];
 						}
@@ -191,28 +154,9 @@ int main(int argc, char** argv) {
 						}else{
 							c_1 < c_2 ? ++hit_map[c_1][c_2][7] : ++hit_map[c_2][c_1][5];
 						}
-
 					}
-			
-	//				if(m_data_1[m] == cur_kmer_loc){
-	//					++cur_hit;
-	//					if(m_data_1[m] != m_data_1[0]){
-	//						all_same = false;
-	//					}
-	//				}
-	//				if(!(m_data_1[m] > m_filter.MASK)){
-	//					++total_unsaturated_rank;
-	//				}
 				}
 			}
-	//		if(cur_hit == 0){
-	//			++unpresented_kmer;
-	//		}
-	//		else{
-	//			++presented_kmer;
-	//		}
-	//		total_hit += cur_hit;
-	//		cur_hit = 0;
 			++cur_kmer_loc;
 			++itr1;
 			++itr2;
@@ -225,12 +169,8 @@ int main(int argc, char** argv) {
 			break;
 		}
 	}
-	//for(int p = 1000; p < 1100; p++){
-	//	std::cout << "hitmap[" << p << "][" << (p+1) << "][...]\t" << hit_map[p][p+1][0] << "\t" << hit_map[p][p+1][1] << "\t" << hit_map[p][p+1][2] << "\t" << hit_map[p][p+1][3] << "\t" << hit_map[p][p+1][4] << "\t" << hit_map[p][p+1][5] << "\t" << hit_map[p][p+1][6] << "\t" << hit_map[p][p+1][7] << std::endl;
-	//}
 	unsigned max_hit;
 	double min_hit_ratio =  0.75;
-	//unsigned total_map_hit;
 	unsigned orient;
 	unsigned hit_index;
 
@@ -239,7 +179,6 @@ int main(int argc, char** argv) {
 	for(unsigned a = 0; a < hit_map.size(); a++){
 		hit_index = 0;
 		max_hit = 0;
-		//total_map_hit = 0;
 		orient = 10;
 		for(unsigned b = 0; b < hit_map.size(); b++){
 			for(unsigned c = 0; c < 8; c+=2){
@@ -254,11 +193,6 @@ int main(int argc, char** argv) {
 			std::cout << a << "\t" << hit_index << "\t" << (orient / 2) << std::endl;
 		}
 	}
-	
-	
-	
-	
-	
 	
 	std::cout << "cur hit " << cur_hit << std::endl;
 	std::cout << "total hit " << total_hit << std::endl;
