35361adc3e8d70cec982ad5798ad7aeb189cc3f8
MurathanGoktas <murathangoktas@gmail.com>
2022-02-01 16:09:19 -0800

    add hits_vec and make refactorization

--- MIBFAnalyze/MIBFAnalyzeReads.cpp
+++ MIBFAnalyze/MIBFAnalyzeReads.cpp
@@ -46,6 +46,69 @@ unsigned getEdgeDistances(map<unsigned,unsigned> &m_pos, unsigned search_pos, un
 	return contig_id;
 }
 
+struct MappedRegion{
+	std::string record_id;
+	unsigned contig_id;
+	unsigned first_contig_pos;
+	unsigned last_contig_pos;
+	unsigned total_hit_pos;
+	bool reverse;
+};
+
+struct ContigHitsStruct{
+	ContigHitsStruct(){}
+
+	ContigHitsStruct(
+		unsigned contig_id,
+		unsigned contig_start_pos,
+		bool reverse_strand,
+		unsigned unsat_hits,
+		unsigned sat_hits
+		):	contig_id(contig_id),
+			contig_start_pos(contig_start_pos),
+			reverse_strand(reverse_strand),
+			unsat_hits(unsat_hits),
+			sat_hits(sat_hits) {}
+
+	unsigned contig_id;
+	unsigned contig_start_pos;
+	bool reverse_strand;
+	unsigned unsat_hits;
+	unsigned sat_hits;
+};
+
+/*
+track_mapping_regions(
+	std::string record_id, unsigned contig_id, unsigned contig_start_dist, unsigned contig_end_dist,
+	unsigned itr1.pos(), bool reverse_strand, bool saturated
+){
+	static vector<MappedRegion> regions;
+	// check if mapping is extending existing mapping region
+
+	// if doesn't extend existing region, create mapping region if satisfies threshold
+
+}
+*/
+
+void add_hit_to_vec(	vector<ContigHitsStruct>& hits_vec, unsigned &contig_id, 
+			unsigned &contig_start_dist, bool &reverse_strand, bool &saturated){
+	bool found = false; 
+	for(auto& cur_struct : hits_vec) {
+		if(cur_struct.contig_id == contig_id && cur_struct.contig_start_pos == contig_start_dist){
+			found = true;
+			if(!saturated){
+				cur_struct.unsat_hits = cur_struct.unsat_hits + 1;
+			} else {
+				cur_struct.sat_hits = cur_struct.sat_hits + 1;
+			}
+			break;
+		}
+	}
+	if(!found){
+		hits_vec.push_back(ContigHitsStruct(contig_id,contig_start_dist,reverse_strand, !saturated ? 1 : 0, saturated ? 1 : 0));
+	}
+}
+
 int main(int argc, char** argv) {
 	printf("GIT COMMIT HASH: %s \n", STRINGIZE_VALUE_OF(GITCOMMIT));
 	//std::scout << GITCOMMIT << std::endl;
@@ -138,6 +201,9 @@ int main(int argc, char** argv) {
 
 	uint contig_id;
 	bool reverse_strand = false;
+	bool saturated = false;
+
+	vector<ContigHitsStruct> hits_vec;
 
 	// read_name	mibf_id		start_pos	length
 	ofstream read_hit_by_pos_file(mibf_path + "_" + base_name + "_read_hit_by_pos.tsv");
@@ -153,27 +219,41 @@ int main(int argc, char** argv) {
 
 				//unsigned cur_true_pos = itr1.pos() + m_pos[contig_id];
 				for(unsigned m = 0; m < m_filter.get_hash_num(); m++){
+					// get contig id by pos
 					c_1 = getEdgeDistances(m_pos,m_data_1[m] & FULL_ANTI_MASK,start_dist_1,end_dist_1); // empty the strand bucket
+
+					// check if contig_id is among target contig ids
 					if(target_contig_mibf_ids.find(c_1) ==  target_contig_mibf_ids.end()){
 						continue;
 					}
-					if(bool((m_data_1[m] & m_filter.ANTI_MASK) > m_filter.STRAND) == itr1.get_strand()){
-						reverse_strand = false;
-					} else {
-						reverse_strand = true;
-					}
-					m_data_1[m] = m_data_1[m] & m_filter.ANTI_STRAND;
 
-					if(m_data_1[m] > m_filter.MASK){ // saturated
-						read_hit_by_pos_file << record.id << "\t" << c_1 << "\t" 
-						<< start_dist_1 << "\t" << itr1.pos() << "\t" << reverse_strand
-						<< "\t1\n";
-					} else {
-						read_hit_by_pos_file << record.id << "\t" << c_1 << "\t" 
-						<< start_dist_1 << "\t" << itr1.pos() << "\t" << reverse_strand
-						<< "\t0\n";
-					}
-				}	
+					// determine if read hits the contig in miBf in forward or reverse orientation
+					reverse_strand = bool(!((m_data_1[m] & m_filter.ANTI_MASK) > m_filter.STRAND) == itr1.get_strand());
+					//determine saturation bit
+					saturated = bool(m_data_1[m] > m_filter.MASK);
+
+					// print for debug
+					read_hit_by_pos_file << record.id << "\t" << c_1 << "\t" 
+					<< start_dist_1 << "\t" << itr1.pos() << "\t" << reverse_strand
+					<< "\t" << saturated << "\n";
+
+					add_hit_to_vec(hits_vec, c_1, start_dist_1, reverse_strand, saturated);
+				}
+				for(auto& str : hits_vec){
+					std::cout << str.contig_id << " " << str.contig_start_pos << " " << str.reverse_strand 
+					<< " " << str.unsat_hits << " " << str.sat_hits << std::endl;
+				}
+				if(hits_vec.size() > 0){
+					std::cout << "\n\n";
+				}
+				hits_vec.clear();
+				/*
+				track_mapping_regions(
+					record.id, c_1, start_dist_1, end_dist_1,
+					itr1.pos(), reverse_strand, saturated
+				);
+				*/	
+
 				// contig_id pos_id unsat_rep_count sat_rep_count
 			}
 			//++cur_kmer_loc;
